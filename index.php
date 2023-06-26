1492043f01a1b53614ce0a598296baafbdf40d96
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-26 18:47:28 -0500

    feat: :sparkles: assignment operators
    Assignment operators allow us to embed data in variables.

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
-    //OPERADORES ARITMETICOS
+    //OPERADORES DE ASIGNACION
+    //El operador básico de asignación es "=".
 
-    //Aritmetics Operators: + - * / ** %
-    $x = 6;
-    $y = 9;
+    $a = ($b = 4) + 5; // ahora $a es igual a 9 y $b se ha establecido en 4.
+    $a = 3;
 
-    echo 'La suma es: '.$x + $y."\n"; 
-    echo 'La resta es: '.$x - $y."\n";
-    echo 'La Multiplicacion es: '.$x * $y."\n";
-    echo 'La Division es: '.$x / $y."\n";
-    echo 'La Esponenciacion es: '.$x ** $y."\n";
-    echo 'El modulo es: '.$x % $y."\n";
+    //Operadores combinados con el de asignacion
+    $a += 5; // establece $a en 8, como si se hubiera dicho: $a = $a + 5;
+    $b = "Hola ";
+    $b .= "ahí!"; // establece $b en "Hola ahí!", al igual que $b = $b . "ahí!";
+
+    //Asignacion por referencia
+    /*Asignación por referencia significa que ambas variables terminan apuntando a los mismos 
+    datos y nada es copiado en ninguna parte. */
+    $a = 3;
+    $b = &$a; // $b es una referencia para $a
+
+    print "$a\n"; // muestra 3
+    print "$b\n"; // muestra 3
+
+    $a = 4; // cambia $a
+
+    print "$a\n"; // muestra 4
+    print "$b\n"; // muestra 4 también, dado que $b es una referencia para $a, la cual ha sido cambiada
 ?> 
