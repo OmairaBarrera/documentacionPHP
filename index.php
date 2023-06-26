6316b7c386b0a935bd158fb8252fdb652c1218d7
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-26 17:58:56 -0500

    feat: :sparkles: type of data
    The types of data that we can find in php are shown.

--- index.php
+++ index.php
@@ -1,28 +1,61 @@
 <?php
-    //CONTANTES
-    /*Una constante es un identificador (nombre) para un valor simple. Como el nombre sugiere, 
-    este valor no puede variar durante la ejecución del script */
-
-    // Declaracion de una constante:
-    /* 
-        1. Utilizamos la funcion define()
-        2. Se declara el nombre de la variables que por convención, los identificadores de 
-        constantes siempre se declaran en mayúsculas
-        3. Seguido de una coma
-        4. Por ultimo se declara el valor de la constante.
+    //TIPOS DE DATOS
+    
+    /*1. Integer. Es un numero
+        Enteros (int): se utilizan para almacenar números enteros sin decimales.
     */
-    define("FINANZA", "mejorar");
+    $numero = 5;
+    $precio = 45000;
 
-    // Nombres de constantes correctos
-    define("FOO", "something");
-    define("FOO2", "something else");
-    define("FOO_BAR", "something more");
+    /*2. Floating point number - floats - doubles - real numbers
+        Punto flotante (float): se utilizan para almacenar números con decimales.
+    */
+    $porcentaje = 0.5;
+    $tasa = 7.5;
 
-    // Nombres de constantes incorrectos
-    define("2FOO",    "something");
+    /*3. String. serie de caracteres
+        Cadenas de texto (string): se utilizan para almacenar texto y caracteres.
+    */
+    $texto = "Bienvenidos a php";
+    $comida = "Arroz con pollo";
+    echo "$texto la comida favorita de php es $comida \n";
 
-    // Esto es válido, pero debe evitarse:
-    // PHP podría cualquier día proporcionar una constante mágica 
-    // que rompiera el script
-    define("__FOO__", "something"); 
+    /*4. Boolean. Tiene dos valores True or False
+        Booleanos (bool): se utilizan para almacenar valores de verdad o falsedad, que se representan 
+        por true o false.
+    */
+    $state = true;
+
+    /*5. Arrays
+        Arreglos (array): se utilizan para almacenar una colección de datos, que pueden ser de 
+        diferentes tipos.
+     */
+    //ambas formas de representar un arreglo son correctas
+    //Primera forma
+    $edades = [20,18,17];
+    //Segunda forma
+    $numeros = array(1,2,3,4);
+
+    /*6. Objetos (object): se utilizan para almacenar instancias de clases, que son definiciones 
+        de objetos.
+    */
+    class foo
+    {
+        function hacer_algo()
+        {
+            echo "Haciendo algo."; 
+        }
+    }
+
+    $bar = new foo;
+    $bar->hacer_algo();
+
+    /*7. Recursos (resource): se utilizan para almacenar referencias a recursos externos, como 
+        conexiones a bases de datos o archivos abiertos.
+        Un valor tipo resource es una variable especial, que contiene una referencia a un recurso 
+        externo
+    */
+    
+    /*8. Nulos (null): se utilizan para representar una variable sin valor o sin definir.*/
+    $var = NULL;  
 ?> 
