552fd97b5b9c6daa1aba4f94f1e28fa8c9c8daaa
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-26 16:06:23 -0500

    feat: :sparkles: Basic structure of a PHP script
    the basic structure necessary for coding in php is shown

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php //Etiqueta de inicio, la cual me indica donde inicia mi proyecto con PHP
+
+    //Aqui va todo el codigo en PHP
+
+//Etiqueta de cierre, indica donde finaliza mi proyecto con PHP
+?> 
