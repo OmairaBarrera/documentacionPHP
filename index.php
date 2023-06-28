0d45076e3ab8245062b6892d8b0b311c056c0bd0
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-27 19:32:52 -0500

    feat: :sparkles: IF condition structure
    It is a control structure that allows decisions to be made based on whether or not a certain condition is met.

--- index.php
+++ index.php
@@ -1,21 +1,40 @@
 <?php
-    //ESTRUCTURAS DE CONTROL
-    /*Las estructuras de control en programación son herramientas que se utilizan para controlar el 
-    flujo de ejecución de un programa
+    //ESTRUCTURAS DE CONDICIONALES
+    /*Las estructuras condicionales son una estructura de control de flujo en programación que 
+    permite que el programa tome decisiones basadas en si se cumple o no una condición */
+
+    //IF
+    /*Se utiliza para definir una acción que se ejecutará si se cumple la condición especificada y 
+    otra acción que se ejecutará si no se cumple. */
+
+    //SINTAXIS
+    /*
+        if(condición){
+            Instrucciones
+        }
     */
 
-    /*  Estructuras de control condicionales: como el condicional "if" que mencionamos anteriormente, y 
-        también el "switch". Estas estructuras permiten que el programa tome decisiones en función de si 
-        se cumple o no una determinada condición.
+    if(10 > 3){
+        //Instrucciones
+        echo "El numero es mayor";
+    };
 
-        Estructuras de control de repetición: también conocidas como bucles o ciclos, permiten que el 
-        programa repita una determinada acción un número de veces determinado o mientras se cumpla una
-        determinada condición. Entre los bucles más comunes se encuentran el "for", el "while" y el 
-        "do-while".
-        
-        Estructuras de control de excepciones: permiten manejar errores o situaciones inesperadas que 
-        puedan ocurrir durante la ejecución del programa. Por ejemplo, las sentencias "try" y "catch" 
-        en Java son estructuras de control de excepciones que permiten manejar errores en tiempo de 
-        ejecución. 
-    */
+    if(10 > 3){
+        //Instrucciones
+        echo "El numero es mayor";
+    } else {
+        //Instrucciones
+        echo "El numero es menor";
+    };
+
+    if(10 > 3){
+        //Instrucciones
+        echo "El numero es mayor";
+    } elseif (10 > 20){
+        //Instrucciones
+        echo "El numero es mayor";
+    } else {
+        //Instrucciones
+        echo "El numero es menor";
+    };
 ?> 
