0c27ca7e65f1fb876a4558760fe15cdbef81a9e7
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-27 19:25:00 -0500

    feat: :sparkles: control structures
    Definitions and types of control structures.

--- index.php
+++ index.php
@@ -1,53 +1,21 @@
 <?php
-    //FUNCION ISSET
-    /*comprueba si una variable o un elemento de un array está definido y no es null. Esta función 
-    devuelve true si la variable o el elemento de array existe y tiene un valor, y false en caso 
-    contrario.
+    //ESTRUCTURAS DE CONTROL
+    /*Las estructuras de control en programación son herramientas que se utilizan para controlar el 
+    flujo de ejecución de un programa
     */
 
-    $miVariable = "Hola";
-    if(isset($miVariable)){
-        echo "La variable esta defiinida y tiene un valor";
-    } else {
-        echo "La Variable no esta definida o no tiene valor";
-    }
-    
-    //FUNCION
-    /*función empty() comprueba si una variable o un elemento de un array está vacío. Esta función 
-    devuelve true si la variable o el elemento de array no tiene un valor definido, o si tiene un 
-    valor que se considera vacío (por ejemplo, una cadena vacía, 0, false, null, un array vacío, 
-    entre otros), y false en caso contrario. 
+    /*  Estructuras de control condicionales: como el condicional "if" que mencionamos anteriormente, y 
+        también el "switch". Estas estructuras permiten que el programa tome decisiones en función de si 
+        se cumple o no una determinada condición.
+
+        Estructuras de control de repetición: también conocidas como bucles o ciclos, permiten que el 
+        programa repita una determinada acción un número de veces determinado o mientras se cumpla una
+        determinada condición. Entre los bucles más comunes se encuentran el "for", el "while" y el 
+        "do-while".
+        
+        Estructuras de control de excepciones: permiten manejar errores o situaciones inesperadas que 
+        puedan ocurrir durante la ejecución del programa. Por ejemplo, las sentencias "try" y "catch" 
+        en Java son estructuras de control de excepciones que permiten manejar errores en tiempo de 
+        ejecución. 
     */
-
-    $miVariable2 = "";
-    if(empty($miVariable2)){
-        echo "La variable esta vacia o no esta definida";
-    } else {
-        echo "La Variable tiene valor";
-    }
-
-    //MAS EJEMPLOS
-    $clientes =[];
-    $clientes2 = array();
-    $clientes3 = array('pedro', 'jose', 'Karen');
-    $cliente = [
-        'nombre' => 'juan',
-        'saldo' => 200,
-    ];
-
-    //Empty - Revisa si un arreglo esta vacio
-    var_dump(empty($clientes));
-    var_dump(empty($clientes2));
-    var_dump(empty($clientes3));
-
-    //Isset - Revisar si un arreglo esta creado o una propiedad esta definida
-    echo "<br>";
-    var_dump(isset($clientes));
-    var_dump(isset($clientes2));
-    var_dump(isset($clientes3));
-    var_dump(isset($cliente));
-
-    //Isset - permite revisar si una propiedad de un arreglo asociativo, existe!
-    var_dump(isset($cliente['nombre']));
-    var_dump(isset($cliente['codigo']));
 ?> 
