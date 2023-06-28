85d434c422250f3abbce51f7e306d172d82d157d
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-27 19:10:08 -0500

    feat: :sparkles: Isset and Empty
    Functions that allow you to review an array

--- index.php
+++ index.php
@@ -1,56 +1,53 @@
 <?php
-    //ARREGLOS ASOCIATIVOS
-    //arreglo asociativo simple
-    $edades = array(
-        "carlos" => 20,
-        "ana" => 25,
-        "juan" => 40,
-    );
+    //FUNCION ISSET
+    /*comprueba si una variable o un elemento de un array está definido y no es null. Esta función 
+    devuelve true si la variable o el elemento de array existe y tiene un valor, y false en caso 
+    contrario.
+    */
 
-    //Se tiene en cuenta que las dos formas de imprimir los arreglos son validos pero distintos
-    //Impresion del dato con concatenacion
-    //Para acceder a un valor de un array asociativo, se utiliza su clave correspondiente
-    echo 'La edad de carlos es '.$edades["carlos"];
-    echo "\n";
-
-    //Impresion del dato con las comillas dobles
-    echo "La edad de juan es {$edades["juan"]} \n";
-
-    //Arreglo asociativo con mas arreglos
-    $personas = array(
-        "Juan" => array(
-            "Apellido" => "Perez",
-            "Edad" => 25,
-            "Identificacion" => 1887293002,
-            "Estudios" => array(
-                "Java" => "Avanzado",
-                "Git" => "Experto",
-            ),
-        ),
-        "Marta" => array(
-            "Apellido" => "Martinez",
-            "Edad" => 20,
-            "Identificacion" => 27283902,
-            "Estudios" => array(
-                "Fluter" => "Experto",
-                "Git" => "Basico",
-            ),
-        ),
-        "Diego" => array(
-            "Apellido" => "Cerecirga",
-            "Edad" => 30,
-            "Identificacion" => 37384933029,
-            "Estudios" => array(
-                "Python" => "Avanzado",
-                "Git" => "Intermedio",
-            ),
-        ),
-    );
-
-    echo "La informacion de Marta es: \n Nombre Completo: Marta {$personas["Marta"]["Apellido"]} \n Edad: {$personas["Marta"]["Edad"]}";
-
-    //Los arrays asociativos utilizando un bucle foreach de la siguiente manera:
-    foreach($edades as $clave => $valor){
-        echo "Clave: " . $clave . ", Valor: " . $valor;
+    $miVariable = "Hola";
+    if(isset($miVariable)){
+        echo "La variable esta defiinida y tiene un valor";
+    } else {
+        echo "La Variable no esta definida o no tiene valor";
     }
+    
+    //FUNCION
+    /*función empty() comprueba si una variable o un elemento de un array está vacío. Esta función 
+    devuelve true si la variable o el elemento de array no tiene un valor definido, o si tiene un 
+    valor que se considera vacío (por ejemplo, una cadena vacía, 0, false, null, un array vacío, 
+    entre otros), y false en caso contrario. 
+    */
+
+    $miVariable2 = "";
+    if(empty($miVariable2)){
+        echo "La variable esta vacia o no esta definida";
+    } else {
+        echo "La Variable tiene valor";
+    }
+
+    //MAS EJEMPLOS
+    $clientes =[];
+    $clientes2 = array();
+    $clientes3 = array('pedro', 'jose', 'Karen');
+    $cliente = [
+        'nombre' => 'juan',
+        'saldo' => 200,
+    ];
+
+    //Empty - Revisa si un arreglo esta vacio
+    var_dump(empty($clientes));
+    var_dump(empty($clientes2));
+    var_dump(empty($clientes3));
+
+    //Isset - Revisar si un arreglo esta creado o una propiedad esta definida
+    echo "<br>";
+    var_dump(isset($clientes));
+    var_dump(isset($clientes2));
+    var_dump(isset($clientes3));
+    var_dump(isset($cliente));
+
+    //Isset - permite revisar si una propiedad de un arreglo asociativo, existe!
+    var_dump(isset($cliente['nombre']));
+    var_dump(isset($cliente['codigo']));
 ?> 
